17f6b8ce3c963c17a654d89a0af074c48179497c
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-10-03 04:38:37 -0700

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    i = 0
+    for char in str:
+        i += 1
+        if ord(char) in range(97, 123):
+            char = chr(ord(char) - 32)
+        if i != len(str):
+            print("{}".format(char), end="")
+        else:
+            print("{}".format(char))
