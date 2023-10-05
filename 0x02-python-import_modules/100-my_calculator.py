a7f484c39f476eaaff443668a655fa816f6c3742
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-10-05 09:47:30 -0700

    Edit_Task7

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    count = len(sys.argv) - 1
-    if(count != 3):
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    operators = ['+', '-', '*', '/']
-    operator = sys.argv[2]
-    if operator not in operators:
-        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    result = 0
-    from calculator_1 import add, sub, mul, div
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
+
+    if sys.argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif sys.argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif sys.argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif sys.argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        result = div(a, b)
-    print("{} {} {} = {}".format(a, operator, b, result))
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
---
+++ 0x02-python-import_modules/100-my_calculator1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    count = len(sys.argv) - 1
+    if(count != 3):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    operators = ["+", "-", "*", "/"]
+    operator = sys.argv[2]
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    result = 0
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    else:
+        result = div(a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
