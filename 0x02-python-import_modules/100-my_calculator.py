4a2344fba1763e1fe3a173b2115c8986a4cf1f53
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-10-05 08:14:41 -0700

    Task6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1
+    import sys
+    count = len(sys.argv) - 1
+    if(count != 3):
+        print("Usage: ./{sys.argv[0]} <a> <operator> <b>")
+        sys.exit(1)
+    operators = ['+', '-', '*', '/']
+    if sys.argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+    if operator == '+':
+        result = calculator_1.add(a, b)
+    elif operator == '-':
+        result = calculator_1.sub(a, b)
+    elif operator == '*':
+        result = calculator_1.mul(a, b)
+    else:
+        result = calculator_1.div(a, b)
+    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
