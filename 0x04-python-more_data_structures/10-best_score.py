84177685a052377d11fe1f806a0a0c3be82619ce
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-10-11 03:59:39 -0700

    Task10_Edit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
         return None
     max_v = 0
     max_k = ""
-    for key,value in a_dictionary.items():
+    for key, value in a_dictionary.items():
         if value > max_v:
             max_v = value
             max_k = key
