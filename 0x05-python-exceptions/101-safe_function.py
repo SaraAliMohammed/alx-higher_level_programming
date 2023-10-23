b0cbbcd68463934d08e30a4373e65f5471d4a164
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-10-23 05:01:04 -0700

    Task8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    """Executes a function safely.
+
+    Args:
+        fct: A pointer to a function
+
+    Returns:
+        The result of the function
+        otherwise None if something happens during the function
+        and prints in stderr the error precede by Exception:
+    """
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
