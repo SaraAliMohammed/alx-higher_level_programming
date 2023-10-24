d576e4ecbfa0863313b4be6050b02cfa50ebc259
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-10-24 02:30:37 -0700

    Task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Defines Square class
+
+    Args:
+        size: Private instance attribute
+    """
+    def __init__(self, size):
+        self.__size = size
