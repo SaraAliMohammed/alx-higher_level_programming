a858551d4c3efed9fbc44f3f3d38a0715f77c276
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-11-01 04:03:54 -0700

    Task4_Edit

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,7 +9,7 @@ def print_square(size):
     if not isinstance(size, int):
         raise TypeError('size must be an integer')
     if size < 0:
-        raise ValueError('size must be a non-negative integer')
+        raise ValueError('size must be >= 0')
     if size == 0:
         return
     for i in range(size):
