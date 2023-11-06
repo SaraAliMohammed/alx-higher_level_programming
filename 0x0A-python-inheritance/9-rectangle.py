bffa9ddb2eed50dc77cb81c586b16a4678126339
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-11-06 03:18:40 -0800

    Task9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This is Rectangle Module """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle Class
+    """
+    def __init__(self, width, height):
+        """Intialize Rectangle instance"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """rectangle area"""
+        return (self.width * self.height)
+
+    def __str__(self):
+        """String method of Rectangle"""
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
