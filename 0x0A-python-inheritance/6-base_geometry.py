2cde2603aa76c41374dc5f6f844703a1aff55bd3
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-11-06 02:46:34 -0800

    Task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is Geometry Module """
+
+
+class BaseGeometry:
+    """
+    BaseGeometry Class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
