5e4c352c3e13806671343efb595d9d209a0d40b2
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-11-06 02:41:40 -0800

    Task4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This is Only sub class of Module """
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks type of object
+    Args:
+        obj: object to be checks.
+        a_class: The class.
+    Returns:
+        True if the object is an instance of a class that inherited
+        (directly or indirectly) from the specified class ;
+        otherwise False.
+    """
+    return (isinstance(obj, a_class) and type(obj) != a_class)
