326f6e6aed25ed5202579a1776480504e8cb312b
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-11-06 03:49:27 -0800

    Task10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is Square Module """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    """
+    Square Class
+    """
+    def __init__(self, size):
+        """Intialize Square instance"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Square area"""
+        return (self.__width * self.__height)
