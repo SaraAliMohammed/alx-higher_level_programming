345e88941d92a623a626b1d71bd5cab4690f84b8
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-11-07 03:01:17 -0800

    Task6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is  Create object from a JSON file Module """
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates an Object from a "JSON file".
+    Args:
+        filename: The name of the file.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.loads(file)
