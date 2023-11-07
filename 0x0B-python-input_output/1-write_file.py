02349266c538816955e8bed09950bb1e058badcb
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-11-07 01:58:59 -0800

    Task1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This is Write to a file Module """
+
+
+def write_file(filename="", text=""):
+    """
+    writes a string to a text file (UTF8).
+    Args:
+        filename: The name of the file.
+        text: Text to be written.
+    Returns: the number of characters written
+    """
+    with open(filename, 'w') as file:
+        file.write(text)
+    return(len(text))
