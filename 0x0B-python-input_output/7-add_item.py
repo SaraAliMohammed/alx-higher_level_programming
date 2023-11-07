c6566c68c873412425d07ca8f456e975173cdbeb
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-11-07 03:12:45 -0800

    Task7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This is Load, add, save Module """
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+arguments = list(sys.argv[1:])
+
+try:
+    data = load_from_json_file("add_item.json")
+except Exception:
+    data = []
+data.extend(arguments)
+save_to_json_file(data, "add_item.json")
