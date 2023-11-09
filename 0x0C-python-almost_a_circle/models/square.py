637eac52735968e6535abf51b141c860c7819805
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-11-09 04:37:55 -0800

    Task10-Square_Class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is a Square Module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+        This is a Square class.
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """String representation of a square"""
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - "
+                f"{self.width}")
