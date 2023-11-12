74dfbf078130f80bdbb325f26ba2a4c833e3137f
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-11-12 02:21:59 -0800

    Task16_Save_to_file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,17 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation
+        of list_objs to a file.
+        Args:
+            cls: The class name.
+            list_objs: A list of instances who inherits of Base.
+        """
+        file_name = "{}.json".format(cls.__name__)
+        if list_objs is not None:
+            list_objs = [obj.to_dictionary() for obj in list_objs]
+        with open(file_name, "w", encoding="utf-8") as file:
+            file.write(cls.to_json_string(list_objs))
