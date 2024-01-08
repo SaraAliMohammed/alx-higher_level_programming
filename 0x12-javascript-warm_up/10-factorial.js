4b68ba88b0f0d03118f334e896020c8d560ff64e
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-01-08 01:47:59 -0800

    Task10. Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (num <= 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
+
+const myNum = parseInt(process.argv[2]);
+if (isNaN(myNum)) {
+  console.log('1');
+} else {
+  console.log(factorial(myNum));
+}
