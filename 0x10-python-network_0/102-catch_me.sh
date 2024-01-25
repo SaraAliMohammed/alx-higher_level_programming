eb3fc2dcfc83345aa9bf8dfd8801be13fae596b3
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-01-25 14:27:34 -0800

    Tasks_Advanced102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -sL -X PUT -H "Origin: You got me!"  -d "user_id=98" 0.0.0.0:5000/catch_me
+
+# Send a request to 0.0.0.0:5000/catch_me with curl and set the User-Agent header to "Holberton School"
+curl -s -X PUT -H "User-Agent: Holberton School" -d "user_id=98" 0.0.0.0:5000/catch_me
