0675cd5ec10843269836cc658392e274fe3282de
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-01-26 02:52:04 -0800

    Task3. Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL
+    - sends a request to the URL
+    - displays the body of the response (decoded in utf-8).
+"""
+import sys
+from urllib import request, error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = request.Request(url)
+    try:
+        with request.urlopen(req) as response:
+            print(response.read().decode("ascii"))
+    except error.HTTPError as e:
+        print("Error code: ", e.code)
