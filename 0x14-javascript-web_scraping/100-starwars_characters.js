641dc83fbe7fda65b49f127a74a48692ba4cb169
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-02-20 03:57:48 -0800

    7. Who was playing in this movie?: Display characters name

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/* Script that prints all characters of a Star Wars movie. */
+const request = require('request');
+const movieId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+request(url, function (er, response, body) {
+  if (er) {
+    console.log(er);
+  } else if (response.statusCode === 200) {
+    const movie = JSON.parse(body);
+    for (const character of movie.characters) {
+      request(character, function (err, body2) {
+        if (err) {
+          console.log(err);
+        }
+        const characterDetails = JSON.parse(body2);
+        console.log(characterDetails.name);
+      });
+    }
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});
